393b0d7a2e86602111c79bf4c87d6f7ad5950d19
Ivan_Luk <>
2024-08-14 17:49:42 +0300

    14.08

--- src/App2.js
+++ src/App2.js
@@ -66,7 +66,6 @@ function App2 () {
   const name = useSelector((state) => state.rootReducer.sign.user.name);
   const role = useSelector((state) => state.rootReducer.sign.user.role);
   const id = useSelector((state) => state.rootReducer.sign.user.id);
-
   
   return (
     <>
